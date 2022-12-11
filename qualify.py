d27bbc17a7d4157eaef4a78a6103484afb0ab941
petycomppety <30414255+petycomppety@users.noreply.github.com>
2022-12-11 18:24:29 +0100

    Update qualify.py
    Added the two lists: contract_list and price_list. The script now prints out only the affected months and price data

--- qualify.py
+++ qualify.py
@@ -1,5 +1,8 @@
 from ib_insync import *
+import pandas as pd
 
+price_list = [] 
+contract_list = []
 # Create an IB object and connect to TWS or Gateway
 ib = IB()
 ib.connect('127.0.0.1', 7497, clientId=1)
@@ -18,7 +21,9 @@ for expiry_date in expiry_dates:
     fut_contract.tradingClass = 'VX'
     fut_contract.lastTradeDateOrContractMonth = expiry_date
     ib.qualifyContracts(fut_contract)
-    print(expiry_date, fut_contract)
+    print(expiry_date, fut_contract.localSymbol)
+    contract_list.append(fut_contract.localSymbol)
+
     ## Request real-time market data for the contract
     # ib.reqMktData(fut_contract, '', False, False)
     ## Print the last price for the contract
@@ -34,7 +39,24 @@ for expiry_date in expiry_dates:
         useRTH=True,
         formatDate=1
     )
+    # Convert the data to a Pandas DataFrame
+    data_df = pd.DataFrame(data)
+    # Get the last row of data using the tail() method
+    last_row = data_df.tail(1)
 
-    print(expiry_date, data)
+    # Access the close field from the last row to get the last price
+    last_price = last_row['close']
+    last_price_value = last_price.values
+    price_list.append(last_price_value[0])
+    print(last_price_value)
+
+i=0
+
+for list in contract_list:
+    print(f'Contract Symbol: {contract_list[i]}')
+    print(f'Price: {price_list[i]}')
+    i = i + 1 
+
+
 
 
